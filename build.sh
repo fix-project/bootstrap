aa0d473c5e616b57054f1d0c8c7913d179aab4e8
Yuhan Deng <yhdeng@stanford.edu>
2023-03-16 19:32:56 -0700

    Add optional flag for controlling number of parallel c-to-elf

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
+PARALLEL=${1:-256}
 SRC_REL=`dirname $0`
 SRC=`realpath ${SRC_REL}`
 OUTPUT=${SRC}/tmp
-
 Tasks='wasm-to-c-fix c-to-elf-fix link-elfs-fix map compile'
 for task in ${Tasks}; do
   echo $task
@@ -11,11 +11,15 @@ for task in ${Tasks}; do
 
   for i in {0..255}
   do
-    ${SRC}/build/src/sys-driver/c-to-elf-sys ${OUTPUT}/${task}/function${i}.c ${OUTPUT}/${task}/function-impl.h ${OUTPUT}/${task}/function.h ${SRC}/build/llvm-project/llvm/lib/clang/16.0.0/include/ ${OUTPUT}/${task}/function${i}.o
+    ${SRC}/build/src/sys-driver/c-to-elf-sys ${OUTPUT}/${task}/function${i}.c ${OUTPUT}/${task}/function-impl.h ${OUTPUT}/${task}/function.h ${SRC}/build/llvm-project/llvm/lib/clang/16.0.0/include/ ${OUTPUT}/${task}/function${i}.o &
+    if [ $(((${i}+1) % ${PARALLEL})) == 0 ]
+    then
+      wait
+    fi
   done
-
-  ${SRC}/build/src/sys-driver/c-to-elf-sys ${OUTPUT}/${task}/function-fixpoint.c ${OUTPUT}/${task}/function-impl.h ${OUTPUT}/${task}/function.h ${SRC}/build/llvm-project/llvm/lib/clang/16.0.0/include/ ${OUTPUT}/${task}/function-fixpoint.o
+  ${SRC}/build/src/sys-driver/c-to-elf-sys ${OUTPUT}/${task}/function-fixpoint.c ${OUTPUT}/${task}/function-impl.h ${OUTPUT}/${task}/function.h ${SRC}/build/llvm-project/llvm/lib/clang/16.0.0/include/ ${OUTPUT}/${task}/function-fixpoint.o &
   wait
+
   LLDSYS="${SRC}/build/src/sys-driver/link-elfs-sys ${OUTPUT}/${task}.o"
 
   for i in {0..255}
