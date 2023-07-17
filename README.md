37da1f5b7182a6867fb9e555e7ab4dfa2deea6b3
Keith Winstein <keithw@cs.stanford.edu>
2023-07-17 15:16:12 -0700

    Add build-all.sh and .gitignore

--- README.md
+++ README.md
@@ -6,11 +6,6 @@ To build this project, you need to first build [fix-project/wasm-toolchain](http
 
 ## Build
 ```
-cmake -S . -B build
-cmake --build build --parallel {# of parallelism}
-cmake -S . -B fix-build -DBUILD_SYS_DRIVER=OFF
-cmake --build fix-build --parallel {# of parallelism}
-./build.sh
-./serialize.sh
+./build-all.sh
 ```
 After building, there should be a `boot/` and a `.fix/` in the source directory of the project. Copy both directories to the source directory of Fix.
---
+++ build-all.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+PARALLEL=${1:-$(nproc)}
+
+cmake -S . -B build
+cmake --build build --parallel $PARALLEL
+cmake -S . -B fix-build -DBUILD_SYS_DRIVER=OFF
+cmake --build fix-build --parallel $PARALLEL
+
+SRC_REL=`dirname $0`
+SRC=`realpath ${SRC_REL}`
+OUTPUT=${SRC}/tmp
+Tasks='wasm-to-c-fix c-to-elf-fix link-elfs-fix map compile'
+for task in ${Tasks}; do
+  echo $task
+  mkdir -p ${OUTPUT}/${task}
+  ${SRC}/build/src/sys-driver/wasm-to-c-sys ${SRC}/fix-build/src/fix-driver/${task}.wasm ${OUTPUT}/${task}
+
+  for i in {0..255}
+  do
+    ${SRC}/build/src/sys-driver/c-to-elf-sys ${OUTPUT}/${task}/function${i}.c ${OUTPUT}/${task}/function-impl.h ${OUTPUT}/${task}/function.h ${SRC}/build/llvm-project/llvm/lib/clang/16.0.0/include/ ${OUTPUT}/${task}/function${i}.o &
+    if [ $(((${i}+1) % ${PARALLEL})) == 0 ]
+    then
+      wait
+    fi
+  done
+  wait
+
+  LLDSYS="${SRC}/build/src/sys-driver/link-elfs-sys ${OUTPUT}/${task}.o"
+
+  for i in {0..255}
+  do
+    LLDSYS+=" "
+    LLDSYS+="${OUTPUT}/${task}/function${i}.o"
+  done
+  eval "${LLDSYS}"
+done
+
+FIX=${SRC}/.fix
+
+mkdir -p ${FIX}
+${SRC}/build/src/serialize/serialize ${SRC}/ ${SRC}/build/llvm-project/llvm/lib/clang/16.0.0/include/
